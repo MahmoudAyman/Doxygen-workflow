13640c31a56dfe9474d99675e3366515bcc87692
MahmoudAyman <mahmoud.ayman07@gmail.com>
2023-11-14 10:28:58 +0200

    updated doxyfile

---
+++ .github/worklfows/test.py
@@ -0,0 +1,16 @@
+class Test:
+    """This class represents a test class."""
+
+    def name(self):
+        """
+        This method represents the 'name' function.
+
+        Explanation about what this function does and its purpose.
+
+        Args:
+            self: The object instance.
+
+        Returns:
+            None
+        """
+        pass
